312103583f0aa7fcaab190ef20e96ad4eef324a4
Roy Garcia <roygarcia201@gmail.com>
2018-08-08 19:15:59 -0700

    PreSubmission of Preference Settings Refactor

--- app/src/main/java/com/example/android/abnproject6/MainActivity.java
+++ app/src/main/java/com/example/android/abnproject6/MainActivity.java
@@ -5,19 +5,22 @@ import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,15 +31,16 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     private static  final String LOG_TAG = MainActivity.class.getName();
     /** news loader id */
     private static final int NEWS_LOADER_ID = 1;
-     /** TextView to be displayed when the list of news is empty */
+    /** TextView to be displayed when the list of news is empty */
     private TextView mEmptyList;
+    /** TextView link to provide information about acquiring TheGuardianAPI key necessary to fetch news*/
+     private TextView mRequestAPI;
     /** Progress bar displayed when loader is loading*/
     private ProgressBar mProgressBar;
 
     /** URL for the news data from https://newsapi.org */
-    private static final String NEWSAPI_REQUEST_URL = "https://content.guardianapis.com/search?q=%22Radiohead%22%20AND%20NOT%20Lana%20del%20Rey&show-tags=contributor&api-key=";
-
-    private String MY_API_KEY = "";
+    //private static final String NEWSAPI_REQUEST_URL = "https://content.guardianapis.com/search?q=%22Radiohead%22%20AND%20NOT%20Lana%20del%20Rey&show-tags=contributor";
+    private static final String NEWSAPI_REQUEST_URL = "https://content.guardianapis.com/search?q=%22Radiohead%22%20AND%20NOT%20Lana%20del%20Rey&show-tags=contributor";
 
     private NewsAdapter mAdapter;
 
@@ -64,9 +68,12 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         });
 
 
-        // Aside from the logo image, an empty list is given when loading
+        // an empty list textview will be used to show messages
         mEmptyList = (TextView) findViewById(R.id.empty_view);
         newsListView.setEmptyView(mEmptyList);
+
+        // textview for api request info
+        mRequestAPI = (TextView) findViewById(R.id.request_api);
 
         // Calls method that handles network connectivity.
         checkNetwork();
@@ -102,7 +109,30 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
     @Override
     public Loader<List<News>> onCreateLoader(int id, Bundle args) {
-        return new NewsLoader(this, NEWSAPI_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // using getString to retrieve the value from the Preferences selected for both order_by and the api_key
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        String apiKey = sharedPrefs.getString(
+                getString(R.string.settings_api_key_key),
+                getString(R.string.settings_api_key_value));
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(NEWSAPI_REQUEST_URL);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        //uriBuilder.appendQueryParameter("api-key", api_key);
+        uriBuilder.appendQueryParameter("api-key", apiKey);
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+
+        return new NewsLoader(this, uriBuilder.toString());
+
     }
 
     /**
@@ -115,8 +145,10 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
         mProgressBar.setVisibility(View.GONE);
 
-        // If not news item retrieved, display "No News found."
+        // If no news item retrieved, display "No News found."
         mEmptyList.setText(R.string.no_news_found);
+        mRequestAPI.setText(R.string.request_api);
+
 
         // garbage collection
         mAdapter.clear();
@@ -130,7 +162,25 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     //When focus is taken away from the app and returned, onLoaderReset is called
     @Override
     public void onLoaderReset(Loader<List<News>> loader) {
-        Log.i(LOG_TAG, "onLoaderReset() called");
         mAdapter.clear();
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    // This method is called whenever an item in the options menu is selected.
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
